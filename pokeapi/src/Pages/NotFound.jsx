5c7392617530ebf7b91d273ca55511b22eb1656f
Arthitphk <128705950+Arthitphk@users.noreply.github.com>
2025-12-13 21:18:36 +0700

    add tailwind css

--- pokeapi/src/Pages/NotFound.jsx
+++ pokeapi/src/Pages/NotFound.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const NotFound = () => {
   return (
-    <div>NotFound 404 </div>
+    <div className=" justify-center items-center "> 
+      <h1 className="text-6xl font-bold text-center text-red-600">404</h1>
+      <p className="text-xl text-center mt-4">Page Not Found</p>
+      <p className="text-center mt-2">Sorry, the page you are looking for does not exist.</p>
+  </div>
+  
   )
 }
