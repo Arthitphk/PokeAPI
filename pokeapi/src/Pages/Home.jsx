d20b9f2609207307701311252616b06a9ced72b3
Arthitphk <128705950+Arthitphk@users.noreply.github.com>
2025-12-13 23:15:04 +0700

    fix logo

--- pokeapi/src/Pages/Home.jsx
+++ pokeapi/src/Pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="flex justify-center">
       <div className="text-4xl font-bold">
-        <img className="w-64 h-auto" src="https://cdn.worldvectorlogo.com/logos/pokemon-23.svg" alt="Logo" />
+        <img className="w-64 h-auto pt-5" src="https://cdn.worldvectorlogo.com/logos/pokemon-23.svg" alt="Logo" />
       </div>
     </div>
   )
