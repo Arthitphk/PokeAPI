e1ac9df7a82d8712129704488aa934ce6cf638f6
Arthitphk <128705950+Arthitphk@users.noreply.github.com>
2025-12-14 18:51:18 +0700

    fix title

---
+++ pokeapi/src/Component/Logo.jsx
@@ -0,0 +1,11 @@
+
+
+export const Logo = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="text-4xl font-bold">
+        <img className="w-64 h-auto pt-5" src="https://cdn.worldvectorlogo.com/logos/pokemon-23.svg" alt="Logo" />
+      </div>
+    </div>
+  )
+}
--- pokeapi/src/Pages/Home.jsx
+++ pokeapi/src/Pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import { Logo } from '../Component/Logo'
 
 const Home = () => {
   return (
-    <div className="flex justify-center">
-      <div className="text-4xl font-bold">
-        <img className="w-64 h-auto pt-5" src="https://cdn.worldvectorlogo.com/logos/pokemon-23.svg" alt="Logo" />
-      </div>
+    <div>
+      <Logo />
     </div>
   )
 }
